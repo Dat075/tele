007986020e48458b1bc81e35331b977c9251931e
Dat075 <94795874+Dat075@users.noreply.github.com>
2026-03-19 01:00:18 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    inject({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+})
